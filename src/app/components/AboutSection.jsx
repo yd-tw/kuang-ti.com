b778ddfcfeece26052af8e857981b786a4280f8f
yd-tw <yd960528@gmail.com>
2024-04-07 20:53:36 +0800

    edit about infomatino

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -5,7 +5,18 @@ import TabButton from "./TabButton";
 
 const TAB_DATA = [
   {
-    title: "Skills",
+    title: "Score",
+    id: "score",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>APCS 大學程式設計先修檢測 - 四級</li>
+        <li>CPE 大學程式能力檢定 - (待補)</li>
+        <li>111學年度 中和高中學習成就測驗 地理科 - 第一名</li>
+      </ul>
+    ),
+  },
+  {
+    title: "skills",
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
@@ -19,29 +30,22 @@ const TAB_DATA = [
     ),
   },
   {
-    title: "Education",
-    id: "education",
+    title: "teams",
+    id: "teams",
     content: (
       <ul className="list-disc pl-2">
-        <li>漳和國中</li>
-        <li>中和高中</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Certifications",
-    id: "certifications",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>FRC</li>
-        <li>程式貓</li>
+        <li>中和高中機器人校隊 FRC8569 - 程式組組長</li>
+        <li>中和高中機器人社 - 活動</li>
+        <li>中和高中資訊研究社 - 社員</li>
+        <li>程式貓社群 - 計畫發起人</li>
+        <li>APCS新創教育團隊 - 測試組組長</li>
       </ul>
     ),
   },
 ];
 
 const AboutSection = () => {
-  const [tab, setTab] = useState("skills");
+  const [tab, setTab] = useState("score");
   const [isPending, startTransition] = useTransition();
 
   const handleTabChange = (id) => {
@@ -53,7 +57,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/yang-kuang-ti.png" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">關於我</h2>
           <p className="text-base lg:text-lg">
@@ -68,25 +72,25 @@ const AboutSection = () => {
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
+              selectTab={() => handleTabChange("score")}
+              active={tab === "score"}
+            >
+              {" "}
+              檢定成績{" "}
+            </TabButton>
+            <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
             >
               {" "}
-              技能{" "}
+              專業技能{" "}
             </TabButton>
             <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
+              selectTab={() => handleTabChange("teams")}
+              active={tab === "teams"}
             >
               {" "}
-              教育{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              證書{" "}
+              團隊參與{" "}
             </TabButton>
           </div>
           <div className="mt-8">
---
+++ src/app/project/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+        <h1 className="text-white">hi</h1>
+    </main>
+  );
+}
