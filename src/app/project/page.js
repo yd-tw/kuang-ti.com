4c93c926ae491aa954f8e1fd0979a31b9a839715
yd-tw <yd960528@gmail.com>
2024-04-09 20:15:08 +0800

    change /project show

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -1,8 +1,21 @@
+import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
+import AboutSection from "@/components/AboutSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import EmailSection from "@/components/EmailSection";
+import Footer from "@/components/Footer";
+import AchievementsSection from "@/components/AchievementsSection";
+import TabInformation from "@/components/TabInformation"
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-        <h1 className="text-white">hi</h1>
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        
+      </div>
+      <Footer />
     </main>
   );
 }
