daf3b9beb3e173872d471f768e64eab7573636fb
yd-tw <yd960528@gmail.com>
2024-04-10 21:57:57 +0800

    add web version in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span>楊光地</span>
-        <p className="text-slate-600">All rights reserved. 2024</p>
+        <div className="">
+          <p className="text-slate-600">All rights reserved. 2024</p>
+          <p className="text-slate-600">Web version: beta-1.0.0</p>
+        </div>
       </div>
     </footer>
   );
