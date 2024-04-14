6a41bd9a6d6f8f3abd06b7adc497604628d877d2
yd-tw <yd960528@gmail.com>
2024-04-14 23:18:01 +0800

    add project page information

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -1,19 +1,13 @@
-import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
-import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
-import AchievementsSection from "@/components/AchievementsSection";
-import TabInformation from "@/components/TabInformation"
-
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        
+        <ProjectsSection/>
       </div>
       <Footer />
     </main>
