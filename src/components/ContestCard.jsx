d4f26c0c7ce2390960b4502e074669dfaa7790e6
yd-tw <yd960528@gmail.com>
2024-04-21 19:58:59 +0800

    Update comtstCard style

--- src/components/ContestCard.jsx
+++ src/components/ContestCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const ContestCard = ({ area, title, rank }) => {
     return (
-        <div className="flex items-center bg-[#221f1f] p-4 rounded-lg">
-            <p className="text-xl text-white mr-8">{area}</p>
-            <p className="text-xl text-white mr-8">{title}</p>
-            <p className="text-xl text-white mr-8">{rank}</p>
-            <div className="ml-auto text-white">
-                <button className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded mr-4">比賽經歷</button>
-                <button className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">官方連結</button>
+        <div className="flex items-center bg-[#221f1f] hover:bg-[#1f2937] p-2 rounded-lg">
+            <p className="text-2xl text-white mr-8">{area}</p>
+            <p className="text-2xl text-white mr-8">{title}</p>
+            <p className="text-2xl text-white mr-8">{rank}</p>
+            <div className="ml-auto text-white font-bold">
+                <button className="bg-orange-900 hover:bg-orange-600 py-1 px-4 rounded mr-4">比賽經歷</button>
+                <button className="bg-orange-900 hover:bg-orange-600 py-1 px-4 rounded mr-4">官方連結</button>
             </div>
         </div>
     );
