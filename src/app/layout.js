2a20e4d8ae297bcdbff0b2dd7013d77230afa2fd
yd-tw <yd960528@gmail.com>
2024-06-19 20:49:10 +0800

    Add GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Analytics from './Analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body className={inter.className}>
+        <Analytics />
+        {children}
+      </body>
     </html>
   )
 }
